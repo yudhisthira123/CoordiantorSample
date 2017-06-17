021e7d2ace92a91e4f93da52c699e88d826da360
yudhisthira.attry@gmail.com <yudhisthira.attry@gmail.com>
2017-06-17 15:03:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yudhisthira/coordiantorsample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.yudhisthira.coordiantorsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        List<Item> itemList = new ArrayList<>();
+
+        itemList.add(new Item("", "name1", "address1"));
+        itemList.add(new Item("", "name2", "address2"));
+        itemList.add(new Item("", "name3", "address3"));
+        itemList.add(new Item("", "name4", "address4"));
+        itemList.add(new Item("", "name5", "address5"));
+        itemList.add(new Item("", "name6", "address6"));
+        itemList.add(new Item("", "name7", "address7"));
+        itemList.add(new Item("", "name8", "address8"));
+        itemList.add(new Item("", "name9", "address9"));
+        itemList.add(new Item("", "name10", "address10"));
+
+
+        recyclerView.setAdapter(new ListAdapter(this, itemList));
+    }
+}
